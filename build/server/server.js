44c603c1cad3224565ca98e5f9930a686d2e3a8d
Mark Lee <markcmlee@gmail.com>
2020-05-27 14:08:50 -0400

    changing from ws to expressws

--- build/server/server.js
+++ build/server/server.js
@@ -1,24 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var path = require("path");
-var mongoose = require("mongoose");
 var express = require("express");
+var WebSocket = require("ws");
+var http = require("http");
+var enableWs = require("express-ws");
 var synapse = require("./synapse/synapse");
-var app = express();
 var PORT = 3000;
-var mongoURI = "mongodb+srv://denskarlet:DS090295170837@cluster0-cwr1z.mongodb.net/test?retryWrites=true&w=majority";
-mongoose
-    .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "synapse",
-})
-    .then(function () { return console.log("Connected to Mongo DB."); })
-    .catch(function (err) { return console.log(err); });
+var app = express();
+var wss = new WebSocket.Server({ port: 3000, path: "/socket" });
+wss.on("connection", function connection(ws) {
+    ws.on("message", function incoming(message) {
+        console.log("received: %s", message);
+        ws.send(message);
+    });
+});
+// express.static("src");
 app.use(express.json());
 app.use("/api", synapse(path.resolve(__dirname, "./resources")));
 app.get("/", function (req, res) {
-    res.send("hello world");
+    // res.send("hello world");
+    res.sendFile(path.resolve(__dirname, "../src/index.html"));
 });
 app.use(function (err, req, res, next) { return res.status(err.status).send(err.serialize()); });
 app.listen(PORT, function () { return console.log("listening on port " + PORT); });
--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,31 @@
 export {};
 const path = require("path");
 const express = require("express");
+const WebSocket = require("ws");
 const synapse = require("./synapse/synapse");
 
+const PORT = 3000;
 const app = express();
 
-const PORT = 3000;
+const wss = new WebSocket.Server({ port: 3001 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+    ws.send(message);
+  });
+});
 
 app.use(express.json());
 
 app.use("/api", synapse(path.resolve(__dirname, "./resources")));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  // res.send("hello world");
+  res.sendFile(path.resolve(__dirname, "../src/index.html"));
 });
 
 app.use((err, req, res, next) => res.status(err.status).send(err.serialize()));
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
-
 module.exports = app;
