8c969291542476ec09043b7feb1127ce2baa3d74
Mark Lee <markcmlee@gmail.com>
2020-05-27 16:41:30 -0400

    sockets working, skeleton of socket test suite

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,16 @@
 export {};
 const path = require("path");
 const express = require("express");
-const WebSocket = require("ws");
+const enableWs = require("express-ws");
 const synapse = require("./synapse/synapse");
 
 const PORT = 3000;
 const app = express();
+enableWs(app);
 
-const wss = new WebSocket.Server({ port: 3001 });
-
-wss.on("connection", function connection(ws) {
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-    ws.send(message);
+app.ws("/", (ws, req) => {
+  ws.on("message", (msg) => {
+    ws.send(msg);
   });
 });
 
@@ -28,4 +26,5 @@ app.get("/", (req, res) => {
 app.use((err, req, res, next) => res.status(err.status).send(err.serialize()));
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
 module.exports = app;
